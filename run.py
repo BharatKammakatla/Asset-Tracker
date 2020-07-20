db46b28923af954379fdfdc40d509f508ff460ef
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-07-20 12:12:41 +0800

    updated config to run in prod

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        # create default user (if not created)
-        if not User.query.filter_by(user_name='admin').first():
-            User.create_user(user='admin',
-                             email='admin@user.com',
-                             password='secret')
-        flask_app.run()
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    # create default user (if not created)
+    if not User.query.filter_by(user_name='admin').first():
+        User.create_user(user='admin',
+                         email='admin@user.com',
+                         password='secret')
+    flask_app.run()
