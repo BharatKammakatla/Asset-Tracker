6349a4f7ccd97e566350ecfd008c9e64b4f8b35a
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-08-18 17:48:38 +0800

    updated init file

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_bootstrap import Bootstrap
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
@@ -17,10 +17,10 @@ login_manager.session_protection = 'strong'
 def create_app(config_type):    #dev, test, prod
 
     app = Flask(__name__)
-    bootstrap = Bootstrap(app)
+
     configuration = os.path.join(os.getcwd(), 'config', config_type+'.py')
     app.config.from_pyfile(configuration)
-
+    bootstrap.init_app(app)
     db.init_app(app)    #bind the database to Flask app
     login_manager.init_app(app)     #Initialize login manager
     bcrypt.init_app(app)    #Initialize bcrypt
